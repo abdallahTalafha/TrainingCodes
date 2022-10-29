d81be566905dc35a765e215b20421a90941e0e9d
3babShogun <abdallah.ta996@gmail.com>
2022-10-29 11:56:47 +0200

    Add SQL Codes file

--- PandasCodes.py
+++ PandasCodes.py
@@ -1,10 +1,18 @@
 import pandas as pd
 
+
+a = [1, 7, 2]
+
+myvar1 = pd.Series(a, index = ["x", "y", "z"])
+
 mydataset = {
   'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
+  'passings': [3, 7, 2] 
 }
 
-myvar = pd.DataFrame(mydataset)
+myvar2 = pd.DataFrame(mydataset)
+pd.options.display.max_rows
 
-print(myvar)
+
+print(myvar1)
+#print(myvar2)
